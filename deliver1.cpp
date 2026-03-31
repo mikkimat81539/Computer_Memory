ee059699d6060867045009aaf6de35bb97069a51
Mikaela Matthews <mikaelamatthews@MacBookAir.lan>
2026-03-30 21:52:27 -0500

    created first file

---
+++ deliver1.cpp
@@ -0,0 +1,5 @@
+/*There is 1 tenant, 1 safe deposit box which can hold 8 letters (1 byte = 8 bits) and you are the mailman (CPU).
+The tenant wants you to store there letters in the mailbox.
+
+Create a program that stores 8 letters*/
+
