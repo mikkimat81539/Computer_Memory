d97e4d45704933cb9513dd6a77cacb719c667770
Mikaela Matthews <mikaelamatthews@MacBookAir.lan>
2026-03-30 23:11:11 -0500

    created second file

---
+++ delivery2.cpp
@@ -0,0 +1,13 @@
+/*Create a slot machine that stores reels as arrays, lets the player place a bet, 
+randomly selects numbers for each reel, and updates the player’s credit based on matches.
+use encapsulation*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+    return 0;
+}
