84086122eab1879584eb5b364957cbe1785a9ec7
Mikaela Matthews <mikaelamatthews@MacBookAir.lan>
2026-03-30 23:28:51 -0500

    declared variables in delivery2

--- delivery2.cpp
+++ delivery2.cpp
@@ -3,8 +3,37 @@ randomly selects numbers for each reel, and updates the player’s credit based 
 use encapsulation*/
 
 #include <iostream>
+#include <ctime>
 
 using namespace std;
+
+class Slot {
+    private:
+        int credit;
+
+        int reel1[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int reel2[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int reel3[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        int payline[3] = {reel1[8], reel2[8], reel3[8]}; // must land on all 7 to win
+
+    public:
+        int bet;
+        int remaining; // credit remaining
+
+    Slot(int bet) { // constructor
+        this -> bet = bet; // place a bet
+        remaining = credit - bet; // remove bet from credit
+
+        // Randomize each reel
+        srand(time(0));
+        int randomNum1 = rand() % 10;
+        int randomNum2 = rand() % 10;
+        int randomNum3 = rand() % 10;
+        int combination[3] = {randomNum1, randomNum2, randomNum3};
+    }
+
+};
 
 int main() {
 
